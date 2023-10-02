d5b8309d128ef750bec225a36e18d2d8edae7b8d
mariangelamarasciuolo <130611642+mariangelamarasciuolo@users.noreply.github.com>
2023-10-02 18:58:39 +0200

    risolto esercizio dell'array
    risolto esercizio dell'array

--- Ex1.java
+++ Ex1.java
@@ -1,24 +1,46 @@
 public class Ex1 {
-    public static void main (String[] args) {
-        int moltResult = moltiplica (6, 6);
-        System.out.println("Moltiplico" + "=" + moltResult );
+    public static void main(String[] args) {
+        int moltResult = moltiplica(6, 6);
+        System.out.println("Moltiplico" + "=" + moltResult);
 
-        String concatResult = concatena ("9",4);
+        String concatResult = concatena("9", 4);
         System.out.println("Concateno:" + concatResult);
 
-        String[] arrayDiStringhe = {"Mela","Pera","Banana","Arancia","Caffè"};
+        String[] arrayDiStringhe = {"Mela", "Pera", "Banana", "Arancia", "Caffè"};
         //---creo l'elemento da aggiungere all'array per metterlo in terza posizione-----
         String aggiungoElemento = "Papaya";
-        int inseriscoPosizione = 2;
+
 
         //mi creo un altro array di dimensioni maggiori
-        int arrayUnPoPiuGrande = arrayDiStringhe.length + 1;
-        String[] arrayAggiornato = new String[arrayUnPoPiuGrande];
-        for(int i = 0; i < inseriscoPosizione; i++) {
+        //int arrayUnPoPiuGrande = arrayDiStringhe.length + 1;
+        //String[] arrayAggiornato = new String[arrayUnPoPiuGrande];
 
+        String[] nuovoArray = modificaArray(arrayDiStringhe, aggiungoElemento);
+        for (int i = 0; i < nuovoArray.length; i++) {
+            System.out.println(nuovoArray[i]);
         }
-        }
-public static int moltiplica(int a, int b) { return a * b;}
-public static String concatena (String a, int b) {return a + b;}
 
+    }
+
+    public static int moltiplica(int a, int b) {
+        return a * b;
+    }
+
+    public static String concatena(String a, int b) {
+        return a + b;
+    }
+
+    public static String[] modificaArray(String[] arrayDiStringhe, String aggiungoElemento) {
+        String[] nuovoArray = new String[arrayDiStringhe.length + 1];
+        int cont = 0;
+        for (int i = 0; i < nuovoArray.length; i++) {
+            if (i == 2) {
+                nuovoArray[i] = aggiungoElemento;
+            } else {
+                nuovoArray[i] = arrayDiStringhe[cont];
+                cont++;
+            }
+        }
+        return nuovoArray;
+    }
 }
